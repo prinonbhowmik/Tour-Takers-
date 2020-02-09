012e480f47be5a7d7e5acbc6d4d3ae45cc8b749d
Prinon <prinonarnob@gmail.com>
2020-02-09 11:42:14 +0600

    phone auth commit

--- app/src/main/java/com/example/tureguideversion1/AccessPhoneNumber.java
+++ app/src/main/java/com/example/tureguideversion1/AccessPhoneNumber.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AccessPhoneNumber extends AppCompatActivity {
 
     private Button next;
+    private EditText phone_Et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,24 @@ public class AccessPhoneNumber extends AppCompatActivity {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         next=findViewById(R.id.nextbtn1);
+        phone_Et = findViewById(R.id.phone_ET);
 
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myintent=new Intent(AccessPhoneNumber.this,CodeVerification.class);
-                startActivity(myintent);
-                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+                String phone = phone_Et.getText().toString().trim();
+
+                if (phone_Et.length()==11){
+                    Intent myintent=new Intent(AccessPhoneNumber.this,CodeVerification.class);
+                    myintent.putExtra("phone", phone);
+                    startActivity(myintent);
+                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                }
+                else{
+                    phone_Et.setError("Enter Valid Phone No");
+                }
 
 
             }
--- app/src/main/java/com/example/tureguideversion1/CodeVerification.java
+++ app/src/main/java/com/example/tureguideversion1/CodeVerification.java
@@ -1,5 +1,6 @@
 package com.example.tureguideversion1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +9,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class CodeVerification extends AppCompatActivity {
+
+    private EditText verification_ET;
     private Button next;
+    private FirebaseAuth auth;
+    private String verificationId;
+    private String phoneNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +37,83 @@ public class CodeVerification extends AppCompatActivity {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         next=findViewById(R.id.nextbtn2);
-
+        verification_ET = findViewById(R.id.verification_ET);
+        auth = FirebaseAuth.getInstance();
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myintent=new Intent(CodeVerification.this,Password.class);
-                startActivity(myintent);
-                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                String code = verification_ET.getText().toString().trim();
+
+                if (verification_ET.length() == 6){
+                    verify(code);
+                }
 
 
             }
         });
+
+        sendOTP();
     }
 
+    private void verify(String code) {
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInwithCredential(credential);
+
+    }
+
+    private void sendOTP() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+88"+phoneNo,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+
+    }
+
+    private void signInwithCredential(PhoneAuthCredential credential) {
+
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(CodeVerification.this,Password.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks =
+            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                @Override
+                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                    String code = phoneAuthCredential.getSmsCode();
+                    if(code != null){
+                        verification_ET.setText(code);
+                        verify(code);
+                    }
+
+                }
+
+                @Override
+                public void onVerificationFailed(@NonNull FirebaseException e) {
+                    Toast.makeText(CodeVerification.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                    super.onCodeSent(s, forceResendingToken);
+
+                    verificationId = s;
+
+                }
+            };
 
     @Override
     public void finish() {
