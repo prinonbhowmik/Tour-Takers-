65bd27fbbf88c5c1cec19ae64138dae31ba70b9d
Prinon <prinonarnob@gmail.com>
2020-03-12 17:07:39 +0600

    tour fragment design

--- app/src/main/java/com/example/tureguideversion1/Fragments/TourFragment.java
+++ app/src/main/java/com/example/tureguideversion1/Fragments/TourFragment.java
@@ -1,5 +1,6 @@
 package com.example.tureguideversion1.Fragments;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.core.view.GravityCompat;
@@ -8,14 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.tureguideversion1.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TourFragment extends Fragment {
+
+    private EditText locationEt,tourDateEt;
 
     public TourFragment() {
         // Required empty public constructor
@@ -28,7 +38,45 @@ public class TourFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tour, container, false);
 
+        locationEt = view.findViewById(R.id.location_Et);
+        tourDateEt = view.findViewById(R.id.tourDate_Et);
+
+        tourDateEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDate();
+            }
+        });
+
 
         return  view;
+    }
+
+    private void getDate() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+                String currentDate = year+"/"+month+"/"+day;
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                Date date = null;
+
+                try {
+                    date = dateFormat.parse(currentDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                tourDateEt.setText(dateFormat.format(date));
+                long milisec = date.getTime();
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), dateSetListener,year,month,day);
+        datePickerDialog.show();
     }
 }
