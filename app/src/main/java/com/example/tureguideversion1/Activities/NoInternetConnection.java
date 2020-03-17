ff4f9afa60793ea6660f51d3e78add1b4d748ca3
imran656565 <imran656565@gmail.com>
2020-03-17 19:27:36 +0600

    Lottie Animation Test

---
+++ app/src/main/java/com/example/tureguideversion1/Activities/NoInternetConnection.java
@@ -0,0 +1,16 @@
+package com.example.tureguideversion1.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tureguideversion1.R;
+
+public class NoInternetConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_internet_connection);
+    }
+}
--- app/src/main/java/com/example/tureguideversion1/Fragments/GuideFragment.java
+++ app/src/main/java/com/example/tureguideversion1/Fragments/GuideFragment.java
@@ -1,5 +1,6 @@
 package com.example.tureguideversion1.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.tureguideversion1.Activities.CreateEvent;
+import com.example.tureguideversion1.Activities.NoInternetConnection;
 import com.example.tureguideversion1.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GuideFragment extends Fragment {
+
+    private Button btn;
 
     public GuideFragment() {
         // Required empty public constructor
@@ -24,6 +30,22 @@ public class GuideFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide, container, false);
+        View view=inflater.inflate(R.layout.fragment_guide, container, false);
+        init(view);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), NoInternetConnection.class));
+
+            }
+        });
+
+
+        return view;
+    }
+
+    private void init(View view) {
+
+        btn=view.findViewById(R.id.internet);
     }
 }
