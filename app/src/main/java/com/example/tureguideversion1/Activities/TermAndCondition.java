6d653ce0ddeb78e2df2b466750df11baee6a6eab
imran656565 <imran656565@gmail.com>
2020-05-29 18:09:04 +0600

    new design for language activity

--- app/src/main/java/com/example/tureguideversion1/Activities/TermAndCondition.java
+++ app/src/main/java/com/example/tureguideversion1/Activities/TermAndCondition.java
@@ -1,12 +1,9 @@
 package com.example.tureguideversion1.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tureguideversion1.R;
 
