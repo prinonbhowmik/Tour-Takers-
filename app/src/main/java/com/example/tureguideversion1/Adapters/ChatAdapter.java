9a4f428e1c4c563aa9c9ece5d8f134568f8c2b0c
AronnoHossain <aarronno@gmail.com>
2020-06-29 10:06:25 +0600

    some changes in comment box

--- app/src/main/java/com/example/tureguideversion1/Adapters/ChatAdapter.java
+++ app/src/main/java/com/example/tureguideversion1/Adapters/ChatAdapter.java
@@ -31,7 +31,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -47,7 +46,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     FirebaseUser fUser;
     FirebaseAuth auth;
     private int row_index = -1, pos, pos2 = -1;
-
+    private long replyDataCount = 0;
     private int timeVisibility = 0;
 
     public ChatAdapter(Context context, List<Chat> mChat) {
@@ -75,6 +74,13 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         Chat chat = mChat.get(position);
+        getReplyData(new replyData() {
+            @Override
+            public void onCallback(HashMap<String, Object> data) {
+                holder.replyData = new HashMap<>();
+                holder.replyData = (HashMap<String, Object>) data;
+            }
+        },chat.getEventID(),chat.getID());
         holder.showMessage.setText(chat.getMessage());
 
         holder.senderName.setText(chat.getSenderName());
@@ -131,39 +137,30 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
                 holder.commentTimeTV.setText(days + " days ago");
         }
 
-        if((holder.commentCounted.getVisibility() == View.VISIBLE) && (holder.replyLayoutView.getVisibility() == View.VISIBLE)) {
-            holder.commentCounted.setVisibility(View.GONE);
-            holder.replyLayoutView.setVisibility(View.GONE);
-        }
-
         DatabaseReference commentRef = FirebaseDatabase.getInstance().getReference().child("eventCommentsReply").child(chat.getEventID()).child(chat.getID());
-        commentRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               if(dataSnapshot.exists()){
-                   long c = dataSnapshot.getChildrenCount()-1;
-                   if(c >1){
-                       holder.commentCounted.setText("Swipe right to view "+c+" more previous replies...");
-                   }else if(c == 1){
-                       holder.commentCounted.setText("Swipe right to view "+c+" more previous reply...");
-                   }else if(c == 0){
-                       holder.commentCounted.setText("Swipe right to view reply...");
-                   }
-               }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+//        commentRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//               if(dataSnapshot.exists()){
+//                   long c = dataSnapshot.getChildrenCount();
+//                   if(c >1){
+//                       holder.commentCounted.setText("Swipe right to view "+c+" more previous replies...");
+//                   }else if(c == 1){
+//                       holder.commentCounted.setText("Swipe right to view "+c+" more previous reply...");
+//                   }
+//               }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
         Query query = commentRef.orderByKey().limitToLast(1);
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if(dataSnapshot.exists()){
-                    holder.commentCounted.setVisibility(View.VISIBLE);
-                    holder.replyLayoutView.setVisibility(View.VISIBLE);
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                         HashMap<String,Object> map = (HashMap<String, Object>) snapshot.getValue();
                         if (!map.get("senderImage").toString().isEmpty()) {
@@ -196,37 +193,121 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             }
         });
 
-
-
-        holder.crl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row_index = holder.getAdapterPosition();
-                notifyDataSetChanged();
-
-            }
-        });
+        if(holder.mainCommentLayout != null) {
+            holder.mainCommentLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getReplyCount(new replyCount() {
+                        @Override
+                        public void onCallback(long l) {
+                            replyDataCount = l;
+                            long c = replyDataCount - 1;
+                            if(c >1){
+                                holder.commentCounted.setText("Swipe right to view "+c+" more previous replies...");
+                            }else if(c == 1){
+                                holder.commentCounted.setText("Swipe right to view "+c+" more previous reply...");
+                            }else if(c == 0){
+                                holder.commentCounted.setText("Swipe right to view reply...");
+                            }
+                        }
+                    },chat.getEventID(),chat.getID());
+                    row_index = holder.getAdapterPosition();
+                    notifyDataSetChanged();
+                }
+            });
+        }
 
         if (position == row_index) {
-            holder.crl.setSelected(true);
+            holder.mainCommentLayout.setSelected(true);
             if (position > pos2 || position < pos2) {
+                if(holder.replyLayoutView.getVisibility() == View.GONE) {
+                    if(holder.replyData != null) {
+                        holder.replyLayoutView.setVisibility(View.VISIBLE);
+                    }else {
+                        holder.replyLayoutView.setVisibility(View.GONE);
+                        holder.commentCounted.setText("Swipe right to reply...");
+                    }
+                }
                 holder.commentTimeTV.setVisibility(View.VISIBLE);
                 pos2 = position;
             } else if (position == pos2) {
+                if(holder.replyLayoutView.getVisibility() == View.VISIBLE) {
+                    holder.replyLayoutView.setVisibility(View.GONE);
+                    holder.commentCounted.setText("Swipe right to reply...");
+                }
                 holder.commentTimeTV.setVisibility(View.GONE);
                 pos2 = -1;
             }
         } else {
-            holder.crl.setSelected(false);
+            if(holder.mainCommentLayout != null) {
+                holder.mainCommentLayout.setSelected(false);
+            }
             holder.commentTimeTV.setVisibility(View.GONE);
+            if(holder.replyLayoutView != null) {
+                holder.replyLayoutView.setVisibility(View.GONE);
+                holder.commentCounted.setText("Swipe right to reply...");
+            }
         }
-        holder.crl.setOnLongClickListener(new View.OnLongClickListener() {
+        if(holder.mainCommentLayout != null) {
+            holder.mainCommentLayout.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    ClipboardManager cm = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                    cm.setText(holder.showMessage.getText());
+                    Toast.makeText(mContext, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+        }
+    }
+
+    public interface replyCount{
+        void onCallback(long l);
+    }
+
+    private void getReplyCount(replyCount replyCount, String eventID, String commentID){
+        DatabaseReference commentRef = FirebaseDatabase.getInstance().getReference().child("eventCommentsReply").child(eventID).child(commentID);
+        commentRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public boolean onLongClick(View v) {
-                ClipboardManager cm = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
-                cm.setText(holder.showMessage.getText());
-                Toast.makeText(mContext, "Copied to clipboard", Toast.LENGTH_SHORT).show();
-                return true;
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    replyCount.onCallback(dataSnapshot.getChildrenCount());
+//                    if(c >1){
+//                        holder.commentCounted.setText("Swipe right to view "+c+" more previous replies...");
+//                    }else if(c == 1){
+//                        holder.commentCounted.setText("Swipe right to view "+c+" more previous reply...");
+//                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public interface replyData {
+        void onCallback(HashMap<String,Object> data);
+    }
+
+    private void getReplyData(replyData replyData, String eventID, String commentID){
+        DatabaseReference commentRef = FirebaseDatabase.getInstance().getReference().child("eventCommentsReply").child(eventID).child(commentID);
+        Query query = commentRef.orderByKey().limitToLast(1);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        HashMap<String,Object> map = (HashMap<String, Object>) snapshot.getValue();
+                        replyData.onCallback(map);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
@@ -245,8 +326,9 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView showMessage, senderName, commentTimeTV, replyerName, replyerComment, commentCounted;
         private CircleImageView profileImageView, replyerImage;
-        private RelativeLayout crl;
+        private RelativeLayout mainCommentLayout;
         private LinearLayout replyLayoutView;
+        private HashMap<String,Object> replyData;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -254,7 +336,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             showMessage = itemView.findViewById(R.id.showMessage);
             profileImageView = itemView.findViewById(R.id.chat_profileImage);
             commentTimeTV = itemView.findViewById(R.id.commentTimeTV);
-            crl = itemView.findViewById(R.id.crl);
+            mainCommentLayout = itemView.findViewById(R.id.mainCommentLayout);
             replyerName = itemView.findViewById(R.id.replyerName);
             replyerComment = itemView.findViewById(R.id.replyerComment);
             commentCounted = itemView.findViewById(R.id.commentCounted);
@@ -268,8 +350,8 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
         return position;
     }
 
-//    @Override
-//    public int getItemViewType(int position) {
+    @Override
+    public int getItemViewType(int position) {
 //        fUser = FirebaseAuth.getInstance().getCurrentUser();
 //        auth = FirebaseAuth.getInstance();
 //        userid = auth.getUid();
@@ -279,7 +361,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
 //        } else {
 //            return MSG_TYPE_LEFT;
 //        }
-//        return position;
-//    }
+        return position;
+    }
 
 }
