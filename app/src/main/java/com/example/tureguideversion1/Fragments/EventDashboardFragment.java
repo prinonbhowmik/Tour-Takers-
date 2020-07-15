9849d68fd5821290d06d1f4580f783cd0c3fb0b8
imran656565 <imran656565@gmail.com>
2020-07-16 01:06:24 +0600

    2 dashboard fragment added

---
+++ app/src/main/java/com/example/tureguideversion1/Fragments/EventDashboardFragment.java
@@ -0,0 +1,23 @@
+package com.example.tureguideversion1.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tureguideversion1.R;
+
+public class EventDashboardFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_dashboard, container, false);
+
+
+    }
+}
